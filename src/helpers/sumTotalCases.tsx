5df83c67d5bdd0a662fe4bee101a06678071c276
Ritik11131 <ritik11131@gmail.com>
2023-04-15 16:07:00 +0530

    some minor changes

--- src/helpers/sumTotalCases.tsx
+++ src/helpers/sumTotalCases.tsx
@@ -1,8 +1,7 @@
 export const sumTotalCases = (arr:any) => {
     const monthlyTotals:any = {};
     for (const date in arr) {
-        const [month, day, year] = date.split('/');
-        // console.log(day);
+        const [month, , year] = date.split('/');
         const total = arr[date];
         const monthYear = `${month}/${year}`;
 
